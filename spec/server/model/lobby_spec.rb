ddae2b910d58ec5359f6a0d2ead86d84839b8047
Boyko Karadzhov <bokovskii@gmail.com>
2016-01-10 17:34:15 +0200

    new game specs

--- spec/server/model/lobby_spec.rb
+++ spec/server/model/lobby_spec.rb
@@ -14,4 +14,17 @@ describe CowsBullsArena::Server::Model::Lobby do
     expect(result.length).to eq(1)
     expect(result[0]).to eq(name)
   end
+
+  it 'should return true for a unique newGame' do
+    name = 'game name'
+    lobby = Lobby.new
+    expect(lobby.new_game(GameOptions.new(name: name))).to be true
+  end
+
+  it 'should return false for a non-unique new_game' do
+    name = 'game name'
+    lobby = Lobby.new
+    lobby.new_game(GameOptions.new(name: name))
+    expect(lobby.new_game(GameOptions.new(name: name))).to be false
+  end
 end
